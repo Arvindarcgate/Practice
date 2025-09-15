69fbad61af511b730ffcc57d8ed6f4d52a1b008b
Arvind Arcgate <arvind.deora@arcgate.com>
2025-09-15 15:32:46 +0530

    Product.ts added

---
+++ frontend/src/pages/Product.tsx
@@ -0,0 +1,12 @@
+// src/pages/Product.ts
+import React from "react";
+
+const Product: React.FC = () => {
+  return (
+    <div>
+      <h2>Product Page</h2>
+    </div>
+  );
+};
+
+export default Product; // ✅ THIS IS REQUIRED
